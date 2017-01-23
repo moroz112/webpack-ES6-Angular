7bc9230cc6fdd397afcd47f4a74f59a7fd70b875
moroz112 <morozalexrpd@gmail.com>
2017-01-23 14:39:54 +0200

    start es6 with webpack

---
+++ app.js
@@ -0,0 +1,7 @@
+import angular from 'angular';
+import uiRouter from 'angular-ui-router';
+
+
+angular.module('app', [
+    uiRouter
+]);
---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+module.exports = {
+    entry: "./app.js",
+    output: {
+        path: __dirname + "/js",
+        filename: "bundle.js"
+    },
+    resolve: {
+        modulesDirectories: ['node_modules']
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.js$/,
+                exclude: /(node_modules|bower_components)/,
+                loader: 'babel-loader',
+                query: {
+                    presets: ['es2015']
+                }
+            },
+            {
+                test: /\.html$/,
+                loader: "html"
+            }
+        ]
+    }
+};
